7acc82c4cc60116bc763c2b5eacb99195351d166
Tracy Cano <tracycano3.14@gmail.com>
2023-12-03 11:04:41 -0800

    Did the multiply, power, and factorial problems, and wrote tests and code

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -9,4 +9,18 @@ end
 
 def sum(numbers)
     numbers.reduce(0) { |sum, number| sum + number }
-  end
+end
+
+def multiply(*num)
+    total = num.reduce(1, :*)
+end
+
+def power(base, exponent)
+     result = base ** exponent
+end
+
+def factorial(number)
+     return 1 if number == 0
+     number * factorial(number - 1)
+end
+
